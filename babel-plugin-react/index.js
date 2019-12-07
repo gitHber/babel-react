3a06d2aad3b5fa9f74d6ae2f9ea662c8151336be
linzihao <435654277@qq.com>
2019-12-07 14:27:26 +0800

    feat: react

--- babel-plugin-react/index.js
+++ babel-plugin-react/index.js
@@ -1,18 +1,21 @@
-module.exports = function({ types: t }) {
+module.exports = function ({ types: t }) {
   return {
     inherits: require("@babel/plugin-syntax-jsx").default,
     visitor: {
       JSXElement(path, state) {
-        console.log("jsxelement", path);
+        console.log("jsxelement", path.get("children")[0].node);
         path.replaceWith(
-          t.callExpression(
-            t.memberExpression(
-              t.identifier("React"),
-              t.identifier("createElement"),
-              false
-            ),
-            t.stringLiteral('path.get("children.0").value')
+          t.expressionStatement(
+            t.callExpression(
+              t.memberExpression(
+                t.identifier("React"),
+                t.identifier("createElement"),
+                false
+              ),
+              [t.stringLiteral(path.get("children")[0].node.value)]
+            )
           )
+
         );
       }
     }
